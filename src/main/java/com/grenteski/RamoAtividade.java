408752c2763d03ad9e10e84aedc61bc83df70c1b
Luiz Fernando <luizgrenfer@gmail.com>
2024-11-23 10:15:09 -0300

    feat:

---
+++ src/main/java/com/grenteski/RamoAtividade.java
@@ -0,0 +1,27 @@
+package com.grenteski;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "ramo_atividade")
+public class RamoAtividade implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+}
